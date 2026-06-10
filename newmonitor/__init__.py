aeccf4e44fbb53984b8ca584948a8beafca5624c
deegoo <71202023+deegoo@users.noreply.github.com>
2026-06-09 23:55:40 -0300

    Remove secret key from Flask app initialization
    retirada senha para acessar MEG

--- newmonitor/__init__.py
+++ newmonitor/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.secret_key = "segredo"
-
 DATA_PATH = "newmonitor/data/tickets.json"
 
 from newmonitor import routes # importtação das rotas no final pq elas precisam do app(@app) rodando para funcionar
